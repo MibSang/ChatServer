1b77483db47f1d3ed6b39c57ce90fa0724b24487
Sanghwa Min <secured8372@gmail.com>
2022-07-20 19:53:44 +0900

    Server/Client basic action,
    TODO: korean encoding apply, ArrayList synchronization.

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,86 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(8081);
-        //server.accept();
-        Client client = new Client("localhost", 8081);
+        if (args.length == 0) {
+            Server server = new Server(8081);
+            server.accept();
+        } else {
+            Client client = new Client("localhost", 8081);
+        }
+    }
+}
+
+class SocketIO {
+    Socket socket;
+    BufferedReader reader;
+    BufferedWriter writer;
+
+    SocketIO(Socket socket) throws IOException {
+        this.socket = socket;
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+    }
+
+    public String readLine() throws IOException {
+        return reader.readLine();
+    }
+
+    public void writeLine(String payload) throws IOException {
+        writer.write(payload+"\n");
+        writer.flush();
     }
 }
 
 class Server extends Thread{
     int port;
     ServerSocket serverSocket;
-    ArrayList<Socket> sockets = new ArrayList<>();
-    class ServerThread {
-        Socket socket;
+    ArrayList<SocketIO> sockets = new ArrayList<>();
+    class ServerThread extends Thread {
+        SocketIO socket;
         InetSocketAddress isa;
+
         ServerThread(Socket socket) {
-            this.socket = socket;
+            try {
+                this.socket = new SocketIO(socket);
+                sockets.add(this.socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             this.isa = (InetSocketAddress) socket.getRemoteSocketAddress();
         }
 
-        void run() {
-            try {
-                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String message = br.readLine();
-
-                if (message.equals("exit")) {
+        public void run() {
+            String message = null;
+            while (true) {
+                try {
+                    message = socket.readLine();
+                } catch (IOException e) {
+                    System.out.println(isa+" disconnected.");
                     sockets.remove(socket);
-                    socket.close();
+                }
+                if (message == null || message.equals("exit") || message.equals("null")) {
+                    System.out.println(isa+" disconnected.");
+                    sockets.remove(socket);
+                    try {
+                        socket.socket.close();
+                    } catch (IOException ignored) { }
                     return;
                 }
 
-                String toSend = isa+": "+message+"\n";
-                System.out.println(toSend);
-                for (Socket s: sockets) {
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-                    bw.write(toSend);
-                    bw.flush();
+                String toSend = isa + ": " + message;
+                System.out.println("\t" + toSend);
+                for (SocketIO s : sockets) {
+                    try {
+                        s.writeLine(toSend);
+                    } catch (IOException e) {
+                        sockets.remove(s);
+                    }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
@@ -56,13 +94,11 @@ class Server extends Thread{
         try {
             serverSocket = new ServerSocket(port);
             while (true) {
-                System.out.println("Waiting for connection...");
                 Socket socket = serverSocket.accept();
                 InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
-                sockets.add(socket);
-                System.out.println("\tConnection established: " + isa);
+                System.out.println("Connection established: " + isa);
                 ServerThread serverThread = new ServerThread(socket);
-                serverThread.run();
+                serverThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -73,17 +109,50 @@ class Server extends Thread{
 class Client {
     int port;
     Socket socket;
+
+    BufferedReader br;
+    BufferedWriter bw;
+    BufferedReader systemRead;
+    ArrayList<String> messages = new ArrayList<>();
+    class ClientReceiver extends Thread {
+        public void run() {
+            String m;
+            while (!socket.isClosed()) {
+                try {
+                    br = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                    m = br.readLine();
+                    System.out.println(m);
+                    messages.add(m);
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                }
+            }
+        }
+    }
+
     Client(String connect_to, int port) {
         this.port = port;
+        System.out.println(System.getProperty("file.encoding"));
         try {
             socket = new Socket(connect_to, port);
             System.out.println("Connected to " + socket.getRemoteSocketAddress());
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            bw.write("hello\n");
-            bw.flush();
-            System.out.println("sent hello");
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            br.readLine();
+            ClientReceiver clientReceiver = new ClientReceiver();
+            bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+            systemRead = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
+            clientReceiver.start();
+            while (!socket.isClosed()) {
+                String payload = systemRead.readLine();
+                System.out.print("\033[H\033[2J"); //clear terminal
+                for (String s: messages)
+                    System.out.println(s);
+
+                bw.write(payload+"\n");
+                bw.flush();
+                if (payload.equals("exit"))
+                    break;
+            }
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
